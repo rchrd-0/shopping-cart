db7dd398c5a661a8c1f1ce52050cf2cd0984c432
richard <richard.ytli+f@gmail.com>
2022-08-08 18:00:44 +0800

    Create product catalog

---
+++ src/assets/productCatalog.js
@@ -0,0 +1,51 @@
+const productCatalog = [
+  {
+    id: null,
+    name: 'Product 001',
+    price: 50,
+  },
+  {
+    id: null,
+    name: 'Product 002',
+    price: 100,
+  },
+  {
+    id: null,
+    name: 'Product 003',
+    price: 80,
+  },
+  {
+    id: null,
+    name: 'Product 004',
+    price: 90,
+  },
+  {
+    id: null,
+    name: 'Product 005',
+    price: 120,
+  },
+  {
+    id: null,
+    name: 'Product 006',
+    price: 120,
+  },
+  {
+    id: null,
+    name: 'Product 007',
+    price: 75,
+  },
+  {
+    id: null,
+    name: 'Product 008',
+    price: 150,
+  },
+  {
+    id: null,
+    name: 'Product 009',
+    price: 180,
+  },
+];
+
+const getCatalog = () => productCatalog;
+
+export default getCatalog;
