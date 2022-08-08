91ead512fe8126b403a86264404798852b49b2b4
richard <richard.ytli+f@gmail.com>
2022-08-08 18:01:09 +0800

    Import product catalog, generate product ids on App mount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import uniqid from 'uniqid';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import './assets/styles/style.css';
+import getCatalog from './assets/productCatalog';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  // Get catalog, generate ids on mount
+  useEffect(() => {
+    const catalog = getCatalog().map((item) => {
+      return {
+        ...item,
+        id: uniqid(),
+      };
+    });
+    setProducts((prevState) => [...catalog]);
+  }, []);
+
   return (
     <div id="app">
       <Router>
