4ff9d04328ec80ec178e84ce792631eb2c198d57
richard <richard.ytli+f@gmail.com>
2022-08-09 14:22:01 +0800

    Map and render cart items

--- src/components/elements/CartItem.js
+++ src/components/elements/CartItem.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
 const CartItem = (props) => {
-  const { cart } = props;
+  const { item } = props;
+  const { name, quantity, price } = item;
   return (
     <div className="cart-card">
       <div className="cart-img" />
       <div className="cart-details">
-        <p className="cart-name">foo</p>
+        <p className="cart-name">{name}</p>
         <div className="cart-quant">
           <button type="button" className="quantity-btn">
             +
           </button>
-          <input type="number" className="input-quantity" />
+          <input
+            type="number"
+            className="input-quantity"
+            defaultValue={quantity}
+          />
           <button type="button" className="quantity-btn">
             -
           </button>
@@ -20,7 +25,7 @@ const CartItem = (props) => {
           Remove
         </button>
       </div>
-      <div className="cart-price">price</div>
+      <div className="cart-price">{price * quantity}</div>
     </div>
   );
 };
--- src/components/views/Cart.js
+++ src/components/views/Cart.js
@@ -2,14 +2,16 @@ import React from 'react';
 import CartItem from '../elements/CartItem';
 
 const Cart = (props) => {
-  const { hideCart } = props;
+  const { cart, hideCart } = props;
   return (
     <div id="cart-wrapper">
       <div id="cart-shadow" onClick={hideCart} />
       <div id="shopping-cart">
         <h1 className="content-header">Your cart</h1>
         <div id="orders">
-          <CartItem />
+          {cart.map((item) => (
+            <CartItem key={item.id} item={item} />
+          ))}
         </div>
       </div>
     </div>
